944acd7cb36bd7d0ad910f3bf55caffa0c2d4cd5
Christina Williams <christinaxtwilliams@gmail.com>
2021-04-30 14:33:40 -0400

    app

--- client/writer/src/App.js
+++ client/writer/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { getCurrentUser } from "./actions/currentUser.js"
+import NavBar from './components/NavBar.js'
+import Home from './components/Home.js'
+import Login from './components/Login.js'
+import Signup from './components/Signup.js'
+import MyStories from './components/MyStories.js'
+import StoryBrief from './components/StoryBrief.js'
+import NewStoryFormWrapper from './components/NewStoryFormWrapper.js'
+import EditStoryFormWrapper from './components/EditStoryFormWrapper.js'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
-export default App;
+  componentDidMount() {
+      this.props.getCurrentUser()
+    }
+
+  render () {
+    const { loggedIn, stories } = this.props
+    return (
+     <div className="App">
+         { loggedIn ? <NavBar home={this.props.home}/> : <Home/> }
+         <Switch>
+           <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
+           <Route exact path='/login' component={Login}/>
+           <Route exact path='/stories' component={MyStories}/>
+           <Route exact path='/stories/new' component={NewStoryFormWrapper}/>
+           <Route exact path='/stories/:id' render={props => {
+
+               const story = stories.find(story => story.id === props.match.params.id)
+               //console.log(story)
+               return <StoryBrief story={story} {...props}/>
+             }
+           }/>
+         <Route exact path='/stories/:id/edit' render={props => {
+
+               const story = stories.find(story => story.id === props.match.params.id)
+              //  dispatch updateForm -> story
+               return <EditStoryFormWrapper story={story} {...props}/>
+             }
+           }/>
+         </Switch>
+       </div>
+     );
+
+   }
+ }
+
+
+ const mapStateToProps = state => {
+   return ({
+     loggedIn: !!state.currentUser,
+     stories: state.myStories
+   })
+ }
+
+ export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
