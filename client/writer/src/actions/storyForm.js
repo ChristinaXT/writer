d9117820fd573d88418bd73d06571eb8ab445f0d
Christina Williams <christinaxtwilliams@gmail.com>
2021-04-30 14:48:16 -0400

    app

---
+++ client/writer/src/actions/storyForm.js
@@ -0,0 +1,32 @@
+// sync actions
+
+export const updateStoryForm = (name, value) => {
+  const formData = { name, value }
+  return {
+    type: "UPDATE_NEW_STORY_FORM",
+    formData
+  }
+}
+
+export const resetStoryForm = () => {
+  return {
+    type: "RESET_NEW_STORY_FORM",
+  }
+}
+
+export const setFormDataForEdit = story => {
+  const storyFormData = {
+    title: story.attributes.title,
+    synopsis: story.attributes.synopsis,
+    epoch: story.attributes.epoch,
+    inciting_incident: story.attributes.inciting_incident,
+    setting: story.attributes.setting,
+    character: story.attributes.character,
+    character_history: story.attributes.character_history,
+    notes: story.attributes.notes
+  }
+  return {
+    type: "SET_FORM_DATA_FOR_EDIT",
+    storyFormData
+  }
+}
